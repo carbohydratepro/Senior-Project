d3d2042b578b90e4f4b6c54d498b6e904ff51e67
carbohydratepro <carbohydratepro@gmail.com>
2023-04-23 20:57:30 +0900

    変換できないファイルのエラーハンドリング

--- convert_to_python3.py
+++ convert_to_python3.py
@@ -10,7 +10,14 @@ def convert_directory_to_python3(directory, backup_extension='.bak'):
                     '2to3', '-W', '--add-suffix={}'.format(backup_extension),
                     '-n', '-W', '-f', 'all', file_path
                 ]
-                subprocess.run(cmd, check=True)
+                try:
+                    subprocess.run(cmd, check=True, capture_output=True)
+                except subprocess.CalledProcessError as e:
+                    print(f"Error while converting {file_path}:")
+                    print("stdout:", e.stdout.decode('utf-8'))
+                    print("stderr:", e.stderr.decode('utf-8'))
+                    print()
+
 
 if __name__ == '__main__':
     target_directory = './deepcoder-master'
