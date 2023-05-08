ee46bc828673209a55d3fcf626e32e8b60ce3c8e
carbohydratepro <carbohydratepro@gmail.com>
2023-05-09 01:06:23 +0900

    データベースを操作するプログラム

---
+++ syntax-analysis/database.py
@@ -0,0 +1,42 @@
+import sqlite3
+
+##データベースの
+class Db():
+    def __init__(self,dbname):
+        self.db=dbname
+
+    def db_create(self, command):
+        conn=sqlite3.connect(self.db)
+        cur = conn.cursor()
+        cur.execute(command)
+        conn.commit()
+        conn.close()
+
+    def db_input(self,article):
+        #値をデータベースに格納
+        conn=sqlite3.connect(self.db)
+        cur = conn.cursor()
+        sql = 'INSERT INTO books (title, author, body) values (?,?,?)'
+        cur.execute(sql, article)
+        conn.commit()
+        cur.close()
+        conn.close()
+
+    def db_output(self):
+        #データベースから値を抽出
+        conn=sqlite3.connect(self.db)
+        cur = conn.cursor()
+        cur.execute('SELECT * from books')
+        data = cur.fetchall()
+        cur.close()
+        conn.close()
+        return data
+
+    def db_update(self, command):
+        conn=sqlite3.connect(self.db)
+        cur = conn.cursor()
+        cur.execute(command)
+
+        conn.commit()
+        cur.close()
+        conn.close()
