542e3ea81903fd047ad889f541b76bd1a5cb6200
carbohydratepro <carbohydratepro@gmail.com>
2023-05-09 01:37:06 +0900

    update

--- syntax-analysis/create_data.py
+++ syntax-analysis/create_data.py
@@ -1,9 +1,10 @@
 import sqlite3
+import os
 from read_csv import read_csv_files, data_output
 
 
 
-def create():
+def create_db():
     conn = sqlite3.connect('./db/coding_problems.db')
     c = conn.cursor()
 
@@ -24,13 +25,33 @@ def create():
     conn.commit()
     conn.close()
 
+def list_files(dir_path, dir_name): # dir_path配下にあるdir_nameディレクトリの中身を配列形式で返す関数
+    directory = dir_path+ "/"+ dir_name
+    file_names = []
+    for entry in os.listdir(directory):
+        file_path = os.path.join(directory, entry)
+        if os.path.isfile(file_path):
+            file_names.append(entry)
+
+    return file_names
+
+
+def create_dataset():
+    exit()
+
 def main():
     dir_path = "./syntax-analysis/Project_CodeNet/metadata"
     num_files = 10
     num_lines = 10
-    data_set = read_csv_files(dir_path, num_files, num_lines)
-    data_output(data_set)
+    data_info = read_csv_files(dir_path, num_files, num_lines)
+    # data_output(data_info)
+
+    data_set = create_dataset(data_info)
 
 
 if __name__ == "__main__":
-    main()
+    # main()
+
+    dir_name = "p00000"
+    dir_path = "./syntax-analysis/Project_CodeNet_Python800"
+    list_files(dir_path, dir_name)
