577e42a1a850ec404796cf7d0fbebfc211e4dbee
carbohydratepro <carbohydratepro@gmail.com>
2023-05-09 00:53:05 +0900

    外部ファイルから使用できるよう修正

--- syntax-analysis/read_csv.py
+++ syntax-analysis/read_csv.py
@@ -2,6 +2,7 @@ import os
 import csv
 
 def read_csv_files(directory, num_files, num_lines):
+    data_set = []
     count_files = 0
     for file_name in os.listdir(directory):
         if count_files >= num_files:
@@ -15,12 +16,18 @@ def read_csv_files(directory, num_files, num_lines):
                 for row in csv_reader:
                     if count_lines >= num_lines:
                         break
-                    print(row)
+                    data_set.append(row)
                     count_lines += 1
             count_files += 1
+    return data_set
+
+def data_output(data):
+    for d in data:
+        print(d)
 
 if __name__ == '__main__':
     dir_path = "./syntax-analysis/Project_CodeNet/metadata"
     num_files = 10
     num_lines = 10
-    read_csv_files(dir_path, num_files, num_lines)
+    data_set = read_csv_files(dir_path, num_files, num_lines)
+    data_output(data_set)
