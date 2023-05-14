78f5b679de376d4cd535372bd0ede34a611af3a7
carbohydratepro <carbohydratepro@gmail.com>
2023-05-15 00:10:14 +0900

    update

--- syntax-analysis/create_data.py
+++ syntax-analysis/create_data.py
@@ -1,30 +1,29 @@
 import sqlite3
 import os
 from tqdm import tqdm
-from read_csv import read_csv_files, data_output
+from read_csv import read_csv_files, read_csv_info, data_output
 from database import Db
 
 
 def isFile(file_name):
     return os.path.isfile(file_name)
 
-def create_db(data):
+def create_db(data, columns):
     dbname = './syntax-analysis/db/coding_problems.db'
-    db=Db(dbname)
+    db = Db(dbname)
     # dbが存在しなければ作成
     if not isFile(dbname):
+        columns_str = ', '.join([f"{col[0]} {col[1]}" for col in columns])
         command = (
-            '''CREATE TABLE propro(
+            f'''CREATE TABLE propro(
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            problem_id STRING,
-            program TEXT
+            {columns_str}
             )'''
         )
         db.db_create(command)
 
     for d in data:
         db.db_input(d)
-
 
 def read_python_file(dir_path, dir_name, file_name): # dir_path配下にあるdir_nameディレクトリのfile_nameの中身を返す関数
     file_path = dir_path+ "/"+ dir_name + "/" + file_name + ".py"
@@ -50,18 +49,28 @@ def create_dataset(data_info):
                 data_set.append([problem_id, answer])
 
     return data_set
-            
+
+def create_only_problem(data_info):
+    pass       
 
 def main():
-    dir_path = "./syntax-analysis/Project_CodeNet/metadata"
-    num_files = 10
-    num_lines = 1000000
-    data_info = read_csv_files(dir_path, num_files, num_lines)
-    print(len(data_info))
-    # data_output(data_info)
+    # dir_path = "./syntax-analysis/Project_CodeNet/metadata"
+    # num_files = 10
+    # num_lines = 1000000
+    # data_info = read_csv_files(dir_path, num_files, num_lines)
+    # # print(len(data_info))
+    # # data_output(data_info)
 
+    data_info = read_csv_info(".\syntax-analysis\Project_CodeNet\metadata\problem_list.csv", 10)
+    print(data_info)
+    exit()
+    # データセット作成
     data_set = create_dataset(data_info)
-    create_db(data_set)
+    
+    columns = [
+        ["problem", "TEXT"]
+    ]
+    create_db(data_set, columns)
 
 def check():
     dbname = './syntax-analysis/db/coding_problems.db'
@@ -71,9 +80,9 @@ def check():
 
 if __name__ == "__main__":
     # データセット作成
-    # main()
+    main()
     
     # データセット確認
-    check()
+    # check()
 
 
