2f2739a52e90d522cc3cc19c59c4fce285944d63
carbohydratepro <carbohydratepro@gmail.com>
2023-05-15 13:32:32 +0900

    update

--- syntax-analysis/del_tag.py
+++ syntax-analysis/del_tag.py
@@ -1,7 +1,10 @@
 import re
 
 def delete_tag(text):
-    tag = '<.+?>'  # タグを表す正規表現
-    text = re.sub(tag, "", text)
-    return text
+    if text is not None:
+        tag = '<.+?>'  # タグを表す正規表現
+        text = re.sub(tag, "", text)
+        return text
+    else:
+        return None
     
