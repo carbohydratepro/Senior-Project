2fded91dc98275dd28365014c7e69fee82ae4905
carbohydratepro <carbohydratepro@gmail.com>
2023-05-16 00:48:24 +0900

    update

--- syntax-analysis/ast_analysis.py
+++ syntax-analysis/ast_analysis.py
@@ -1,6 +1,18 @@
 import ast
 
-with open("solution.py", "r") as f:
-    code = f.read()
+def convert_ast(content):
+    try:
+        tree = ast.parse(content)
+        return tree
+    except:
+        return None
+        
+def main():
+    with open("./syntax-analysis/del_tag.py", "r") as f:
+        code = f.read()
 
-tree = ast.parse(code)
+    tree = convert_ast(code)
+    print(tree)
+
+if __name__ == "__main__":
+    main()
