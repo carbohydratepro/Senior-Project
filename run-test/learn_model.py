8d996beeb76a129156f833c8bc3f17edb1a6f8bc
carbohydratepro <carbohydratepro@gmail.com>
2023-05-24 06:45:32 +0900

    モデルを学習するプログラム

--- run-test/learn_model.py
+++ run-test/learn_model.py
@@ -22,7 +22,7 @@ class Seq2SeqModel(nn.Module):
         super(Seq2SeqModel, self).__init__()
         self.encoder = BertModel.from_pretrained('bert-base-uncased')
         self.decoder = nn.LSTM(768, 768, batch_first=True)
-        self.fc = nn.Linear(768, vocab_size)  # vocab_size：ボキャブラリサイズ（トークンの数）
+        self.fc = nn.Linear(768, 30522)  # vocab_size=30522：ボキャブラリサイズ（トークンの数）
 
     def forward(self, input_ids, decoder_input):
         encoder_output = self.encoder(input_ids)[0]
@@ -30,21 +30,29 @@ class Seq2SeqModel(nn.Module):
         output = self.fc(decoder_output)
         return output
 
+def main():
+    # 初期情報の設定
+    device = "cuda"
+    num_epochs = 10
+    vocab_size = 30522
 
-# データローダの準備
-dataset = Seq2SeqDataset(problems, programs)  # problemsとprogramsはそれぞれ問題文と正答プログラムのリスト
-dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
+    # データローダの準備
+    dataset = Seq2SeqDataset(problems, programs)  # problemsとprogramsはそれぞれ問題文と正答プログラムのリスト
+    dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
 
-# モデルとオプティマイザの準備
-model = Seq2SeqModel().to(device)  # deviceはあなたのハードウェア（CPUまたはGPU）
-optimizer = Adam(model.parameters(), lr=0.001)
+    # モデルとオプティマイザの準備
+    model = Seq2SeqModel().to(device)  # deviceはハードウェア（CPUまたはGPU）
+    optimizer = Adam(model.parameters(), lr=0.001)
 
-# モデルの訓練
-for epoch in range(num_epochs):  # num_epochsはエポック数
-    for problems, programs in dataloader:
-        optimizer.zero_grad()
-        output = model(problems.to(device), programs.to(device))
-        loss = nn.CrossEntropyLoss()(output.view(-1, vocab_size), programs.view(-1))
-        loss.backward()
-        optimizer.step()
-    print('Epoch:', epoch, 'Loss:', loss.item())
+    # モデルの訓練
+    for epoch in range(num_epochs):  # num_epochsはエポック数
+        for problems, programs in dataloader:
+            optimizer.zero_grad()
+            output = model(problems.to(device), programs.to(device))
+            loss = nn.CrossEntropyLoss()(output.view(-1, vocab_size), programs.view(-1))
+            loss.backward()
+            optimizer.step()
+        print('Epoch:', epoch, 'Loss:', loss.item())
+
+if __name__ == "__main__":
+    main()
