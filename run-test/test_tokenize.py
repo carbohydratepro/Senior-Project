a76c0de534ab83fa604f0509f22ca37f8228c23b
carbohydratepro <carbohydratepro@gmail.com>
2023-05-28 22:51:18 +0900

    tokenizerの動作テスト用

--- run-test/test_tokenize.py
+++ run-test/test_tokenize.py
@@ -1,4 +1,5 @@
 from transformers import AutoTokenizer
+from transformers.models.gpt2.tokenization_gpt2 import bytes_to_unicode
 
 python_code = r"""def say_hello():
     print("hello, World!")
@@ -8,3 +9,8 @@ say_hello()
 """
 tokenizer = AutoTokenizer.from_pretrained("gpt2")
 print(tokenizer(python_code).tokens())
+
+byte_to_unicode_map = bytes_to_unicode()
+unicode_to_byte_map = dict((v, k) for k, v in byte_to_unicode_map.items())
+base_vocab = list(unicode_to_byte_map.keys())
+
