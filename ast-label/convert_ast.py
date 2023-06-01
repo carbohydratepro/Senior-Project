c11e258f27a8372ade443ca8d43e4fd0e8ab4bba
carbohydratepro <carbohydratepro@gmail.com>
2023-06-02 00:43:38 +0900

    update

--- ast-label/convert_ast.py
+++ ast-label/convert_ast.py
@@ -1,29 +1,5 @@
 import ast
 
-# コードをASTに変換
-def convert_to_ast(content):
-    try:
-        tree = ast.parse(content)
-        return tree
-    except Exception as e:
-        print("Error in parsing:", e)
-        return None
-
-# ASTから特徴ベクトルへ変換
-def ast_to_feature_vector(ast_tree, max_length):
-    # DFSを使用してASTツリーを訪問し、ノードタイプ名を収集
-    nodes = list(ast.walk(ast_tree))
-    feature_vector = [type(n).__name__ for n in nodes]
-
-    # 特徴ベクトルの長さがmax_lengthを超えている場合は切り捨て
-    if len(feature_vector) > max_length:
-        feature_vector = feature_vector[:max_length]
-    # 特徴ベクトルの長さがmax_lengthに満たない場合はパディング
-    else:
-        feature_vector += ['<PAD>'] * (max_length - len(feature_vector))
-
-    return feature_vector
-
 class CustomVisitor(ast.NodeVisitor):
     def __init__(self):
         self.function_defs = 0
@@ -37,6 +13,25 @@ class CustomVisitor(ast.NodeVisitor):
         self.function_calls += 1
         self.generic_visit(node)
 
+
+# コードをASTに変換
+def convert_to_ast(content):
+    try:
+        tree = ast.parse(content)
+        return tree
+    except Exception as e:
+        print("Error in parsing:", e)
+        return None
+
+# ASTから特徴ベクトルへ変換
+def ast_to_feature_vector(ast_tree):
+    # DFSを使用してASTツリーを訪問し、ノードタイプ名を収集
+    nodes = list(ast.walk(ast_tree))
+    feature_vector = [type(n).__name__ for n in nodes]
+
+    return nodes, feature_vector
+
+
 def write_to_file(label, data):
     for i, info in enumerate(data, 1):
         with open(f'info{i}.txt', 'w') as f:
@@ -46,7 +41,7 @@ def write_to_file(label, data):
 
 
 def main():
-    label = ["ファイル名", "定義されている関数の数", "関数が呼び出されている回数", "DFS", "AST", "プログラム"]
+    label = ["ファイル名", "定義されている関数の数", "関数が呼び出されている回数", "DFS", "DFS_vector", "AST", "プログラム"]
     data = []
 
     # プログラムのパス
@@ -58,26 +53,31 @@ def main():
               ".\syntax-analysis\Project_CodeNet_Python800\p00000\s080762108.py"]
     
     # 与えられた情報を格納してデータを一次元配列で返す関数
-    def analysis(code):
+    def analysis(path, code):
         # AST変換
         tree = convert_to_ast(code)
 
+        # ASTを訪問するオブジェクトを作成
         visitor = CustomVisitor()
         visitor.visit(tree)
 
-        print(f"Function definitions: {visitor.function_defs}") #定義されている関数の数
-        print(f"Function calls: {visitor.function_calls}") #関数が呼び出されている回数
+        function_definitions = visitor.function_defs
+        function_calls = visitor.function_calls
 
-        print(ast.dump(tree, indent=2))
+        program_ast = ast.dump(tree, indent=2)
 
+        nodes, feature_vector = ast_to_feature_vector(program_ast)
+
+        return [path, function_definitions, function_calls, nodes, feature_vector, program_ast, code]
     
     for path in pathes:
         with open(path, mode='r', encoding='utf-8') as f:
             code = f.read()
-            print(code)
-            analysis(code)
+            data.append(analysis(path, code))
 
+    # データをテキストファイルに書き込み
     write_to_file(label, data)
+
 
 
 if __name__ == "__main__":
