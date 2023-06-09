7b7997e1c6293d86eab75ad5032060efee6e8f31
carbohydratepro <carbohydratepro@gmail.com>
2023-06-09 17:15:48 +0900

    学習したモデルを簡易的に評価するプログラムを追加

--- ast-label/create_bert_model.py
+++ ast-label/create_bert_model.py
@@ -19,7 +19,7 @@ def eval(datasets, test_data_num=0):
 
     # 学習用データの定義
     train_programs = datasets_programs[0:train_data_num]
-    trains_labels = datasets_labels[0:train_data_num]
+    train_labels = datasets_labels[0:train_data_num]
 
     # テストデータの定義
     test_programs = datasets_programs[train_data_num:-1]
@@ -39,39 +39,57 @@ def eval(datasets, test_data_num=0):
         outputs = model(**inputs)
         train_features.append(outputs.last_hidden_state[0].mean(0).detach().numpy())
     train_features = np.array(train_features)
-
-    # プログラムから特徴量を抽出（テスト用データ）
-    logging.info("Extracting features from test programs...")
-    test_features = []
-    for test_program in tqdm(test_programs):
-        inputs = tokenizer(test_program, return_tensors='pt', truncation=True, max_length=512)
-        outputs = model(**inputs)
-        test_features.append(outputs.last_hidden_state[0].mean(0).detach().numpy())
-    test_features = np.array(test_features)
-
-    
+  
 
     # 2. モデルの学習
     # ランダムフォレスト分類器を学習
     logging.info("Training the classifier...")
     clf = RandomForestClassifier()
-    clf.fit(features, labels)
+    clf.fit(train_features, train_labels)
 
     # 3. 予測
     # 新しいプログラムから特徴量を抽出し、ラベルを予測
     logging.info("Predicting the label of a new program...")
-    new_program = "new program"
-    inputs = tokenizer(new_program, return_tensors='pt', truncation=True, max_length=512)
-    outputs = model(**inputs)
-    new_features = outputs.last_hidden_state[0].mean(0).detach().numpy().reshape(1, -1)
+    test_features = []
+    for test_program in tqdm(test_programs):
+        inputs = tokenizer(test_program, return_tensors='pt', truncation=True, max_length=512)
+        outputs = model(**inputs)
+        test_features.append(outputs.last_hidden_state[0].mean(0).detach().numpy().reshape(1, -1))
+    test_features = np.array(test_features)
 
-    prediction = clf.predict(new_features)
-    print("Predicted label:", prediction)
+
+    # 4. 評価
+    # 評価結果を格納する配列を定義
+    evaluation_value = {'correct':0, 'incorrect':0}
+    eval_results = [] #problem_id、正解数、不正解数、データ数
+    for test_feature, test_label in tqdm(zip(test_features, test_labels)):
+        prediction = clf.predict(test_feature)
+        
+        if test_label not in [eval_result[0] for eval_result in eval_results]:
+            eval_results.append([test_label, 0, 0, 0])
+
+        if prediction == test_label:
+            evaluation_value['correct'] += 1
+            eval_results[[eval_result[0] for eval_result in eval_results].index(test_label)][1] += 1
+            eval_results[[eval_result[0] for eval_result in eval_results].index(test_label)][3] += 1
+        else:
+            evaluation_value['incorrect'] += 1
+            eval_results[[eval_result[0] for eval_result in eval_results].index(test_label)][2] += 1
+            eval_results[[eval_result[0] for eval_result in eval_results].index(test_label)][3] += 1
+
+    for eval_result in eval_results:
+        print(eval_result[0], ':', eval_result[3], ":", "{:.2f}".format(eval_result[1]/(eval_result[1]+eval_result[2])*100))
+        
+    print(len(eval_results))
+
+    rate = evaluation_value['correct'] / (evaluation_value['correct']+evaluation_value['incorrect']) * 100
+    print(rate)
+
 
 def main():
     # 初期情報の設定
-    data_num = 1000
-    test_data_num = 200
+    data_num = 100
+    test_data_num = int(data_num * 0.5)
 
     # データセットの読み込み
     datasets = read_data(data_num)
