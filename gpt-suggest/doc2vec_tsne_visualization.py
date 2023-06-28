58a98052dc96ff6cc793935468bf7c475abf4c87
carbohydratepro <carbohydratepro@gmail.com>
2023-06-28 16:26:01 +0900

    プログラムの改良

--- gpt-suggest/doc2vec_tsne_visualization.py
+++ gpt-suggest/doc2vec_tsne_visualization.py
@@ -1,12 +1,18 @@
 from gensim.models.doc2vec import Doc2Vec, TaggedDocument
 from sklearn.manifold import TSNE
 from janome.tokenizer import Tokenizer
+from database import Db
+from tqdm import tqdm
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib as mpl
+import logging
+import random
 
-
-# # 文書とそのタイトル
-
+# ログの設定
+logging.basicConfig(level=logging.INFO)
+plt.rcParams['font.family'] = 'MS Gothic'
+plt.rcParams['font.size'] = 5  # フォントサイズを10に設定
 
 # # AIと量子コンピューティングの二つのトピック
 # documents = [
@@ -41,21 +47,37 @@ import matplotlib.pyplot as plt
 #     ("The Role of Quantum Mechanics in Computing", "Quantum Computing", "量子力学がコンピューティングに果たす役割"),
 #     ("Quantum Computing for Solving Complex Problems", "Quantum Computing", "複雑な問題解決のための量子コンピューティング"),
 # ]
+def get_data(dbname):
+    command = 'SELECT * from theses'
+    db = Db(dbname)
+    data = db.db_output(command)
+    return data
 
-
+# データセットからランダムに抽出する関数
+def select_random_elements(array, n):
+    if n > len(array):
+        raise ValueError("n is greater than the length of the array.")
+    
+    random_elements = random.sample(array, n)
+    return random_elements
+    
 def tsne_plt(documents):
     t = Tokenizer()
 
     # TaggedDocumentの形にする
+    logging.info("create tagged doccument...")
     tagged_documents = [TaggedDocument(words=[token.surface for token in t.tokenize(d[1])], tags=[d[0]]) for d in documents]
 
     # Train the Doc2Vec model
-    model = Doc2Vec(tagged_documents, vector_size=50, window=2, min_count=1, workers=4)
+    logging.info("Train the Doc2Vec model...")
+    model = Doc2Vec(tagged_documents, dm=0, vector_size=300, window=15, alpha=.025,min_alpha=.025, min_count=1, sample=1e-6, workers=4, epochs=100)
 
     # Get the document vectors from the model
+    logging.info("Get the document vectors from the model")
     X = model.dv.vectors
 
     # Apply t-SNE to the document vectors
+    logging.info("Apply t-SNE to the document vectors")
     tsne = TSNE(n_components=2, random_state=0, perplexity=10)  # Adjust perplexity value here
 
     X_tsne = tsne.fit_transform(X)
@@ -71,41 +93,55 @@ def tsne_plt(documents):
         plt.annotate(titles[i], xy=(X_tsne[i, 0], X_tsne[i, 1]), xytext=(5, 2), 
                     textcoords='offset points', ha='right', va='bottom')
     plt.show()
+    plt.savefig(f"./gpt-suggest/picture/figure3.png")
     
     
 def main():
-    documents = [
-        ("Machine Learning in Automated Driving Systems", "この研究では、機械学習を用いた自動運転システムについて検討する。"),
-        ("Image Recognition with Deep Learning", "深層学習を用いた画像認識技術が近年注目されている。"),
-        ("The Role of AI in Future Industries", "AIは未来の産業をリードする重要な要素となる。"),
-        ("Natural Language Processing and Its Applications", "自然言語処理はAIの重要な研究分野であり、日常生活への応用が期待されている。"),
-        ("Cyber Security in The Age of AI", "AI時代のサイバーセキュリティについて考察する。"),
-        ("The Impact of AI on Employment", "AIが雇用に及ぼす影響についての調査研究"),
-        ("Understanding The Basics of Neural Networks", "ニューラルネットワークの基礎について学ぶ"),
-        ("The Rise of Quantum Computing", "量子コンピューティングの台頭について"),
-        ("The Influence of Blockchain Technology on Finance", "ブロックチェーン技術が金融業界に及ぼす影響"),
-        ("Augmented Reality in Education", "教育現場における拡張現実(AR)技術の活用"),
-        ("Virtual Reality in Gaming Industry", "ゲーム業界におけるバーチャルリアリティ(VR)の利用状況"),
-        ("The Evolution of Cloud Computing", "クラウドコンピューティングの進化について"),
-        ("Big Data and Its Impact on Business", "ビッグデータがビジネスに与える影響について"),
-        ("The Emergence of 5G Technology", "5G技術の出現とその意味するもの"),
-        ("The Role of Data Science in Decision Making", "意思決定におけるデータサイエンスの役割"),
-        ("Understanding The Internet of Things (IoT)", "インターネット・オブ・シングス（IoT）について理解する"),
-        ("The Use of AI in Healthcare", "医療現場でのAIの利用について"),
-        ("The Potential of Robotics in Manufacturing", "製造業におけるロボティクスの可能性"),
-        ("The Impact of Digital Transformation", "デジタル変革が社会に及ぼす影響"),
-        ("The Future of FinTech", "フィンテックの未来について"),
-        ("Advances in Facial Recognition Technology", "顔認識技術の進歩について"),
-        ("The Role of Smart Cities in Sustainable Development", "持続可能な開発におけるスマートシティの役割"),
-        ("Artificial Intelligence in Retail", "小売業界における人工知能の活用"),
-        ("The Use of Machine Learning in Agriculture", "農業における機械学習の利用"),
-        ("Predictive Analytics in Business", "ビジネスにおける予測分析の活用"),
-        ("AI Ethics and Governance", "AIの倫理とガバナンスについて"),
-        ("Robotics in Elderly Care", "高齢者ケアにおけるロボティクスの活用"),
-        ("The Application of AI in Cybersecurity", "サイバーセキュリティにおけるAIの応用"),
-        ("Challenges in Implementing AI Solutions", "AIソリューションを実装する際の課題"),
-        ("The Future of Autonomous Vehicles", "自動運転車の未来について")
-    ]
+    # documents = [
+    #     ("Machine Learning in Automated Driving Systems", "この研究では、機械学習を用いた自動運転システムについて検討する。"),
+    #     ("Image Recognition with Deep Learning", "深層学習を用いた画像認識技術が近年注目されている。"),
+    #     ("The Role of AI in Future Industries", "AIは未来の産業をリードする重要な要素となる。"),
+    #     ("Natural Language Processing and Its Applications", "自然言語処理はAIの重要な研究分野であり、日常生活への応用が期待されている。"),
+    #     ("Cyber Security in The Age of AI", "AI時代のサイバーセキュリティについて考察する。"),
+    #     ("The Impact of AI on Employment", "AIが雇用に及ぼす影響についての調査研究"),
+    #     ("Understanding The Basics of Neural Networks", "ニューラルネットワークの基礎について学ぶ"),
+    #     ("The Rise of Quantum Computing", "量子コンピューティングの台頭について"),
+    #     ("The Influence of Blockchain Technology on Finance", "ブロックチェーン技術が金融業界に及ぼす影響"),
+    #     ("Augmented Reality in Education", "教育現場における拡張現実(AR)技術の活用"),
+    #     ("Virtual Reality in Gaming Industry", "ゲーム業界におけるバーチャルリアリティ(VR)の利用状況"),
+    #     ("The Evolution of Cloud Computing", "クラウドコンピューティングの進化について"),
+    #     ("Big Data and Its Impact on Business", "ビッグデータがビジネスに与える影響について"),
+    #     ("The Emergence of 5G Technology", "5G技術の出現とその意味するもの"),
+    #     ("The Role of Data Science in Decision Making", "意思決定におけるデータサイエンスの役割"),
+    #     ("Understanding The Internet of Things (IoT)", "インターネット・オブ・シングス（IoT）について理解する"),
+    #     ("The Use of AI in Healthcare", "医療現場でのAIの利用について"),
+    #     ("The Potential of Robotics in Manufacturing", "製造業におけるロボティクスの可能性"),
+    #     ("The Impact of Digital Transformation", "デジタル変革が社会に及ぼす影響"),
+    #     ("The Future of FinTech", "フィンテックの未来について"),
+    #     ("Advances in Facial Recognition Technology", "顔認識技術の進歩について"),
+    #     ("The Role of Smart Cities in Sustainable Development", "持続可能な開発におけるスマートシティの役割"),
+    #     ("Artificial Intelligence in Retail", "小売業界における人工知能の活用"),
+    #     ("The Use of Machine Learning in Agriculture", "農業における機械学習の利用"),
+    #     ("Predictive Analytics in Business", "ビジネスにおける予測分析の活用"),
+    #     ("AI Ethics and Governance", "AIの倫理とガバナンスについて"),
+    #     ("Robotics in Elderly Care", "高齢者ケアにおけるロボティクスの活用"),
+    #     ("The Application of AI in Cybersecurity", "サイバーセキュリティにおけるAIの応用"),
+    #     ("Challenges in Implementing AI Solutions", "AIソリューションを実装する際の課題"),
+    #     ("The Future of Autonomous Vehicles", "自動運転車の未来について")
+    # ]
+    
+    documents = []
+
+    dbname = './gpt-suggest/db/tuboroxn.db'
+    dataset = get_data(dbname)
+    for data in tqdm(dataset):
+        if len(data[-1]) > 200 and data[-2] != r"卒業論文\n\n論文題目\n\n([\s\S]*?)\n\n":
+            documents.append([data[-2], data[-1]]) # .replace("\n", "").replace("・", "")
+        else:
+            pass
+
+    
+    documents = select_random_elements(documents, 50)
     tsne_plt(documents)
     
 if __name__ == "__main__":
