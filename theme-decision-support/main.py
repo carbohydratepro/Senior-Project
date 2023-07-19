05852ea752a546eb2e3583eba0441b77a95642c9
carbohydratepro <carbohydratepro@gmail.com>
2023-07-20 05:33:29 +0900

    とりあえず完成

--- theme-decision-support/main.py
+++ theme-decision-support/main.py
@@ -2,6 +2,13 @@ from transformers import AutoTokenizer, AutoModel, BertJapaneseTokenizer, BertMo
 import torch
 from scipy.spatial.distance import cosine
 from database import Db
+from tqdm import tqdm
+import logging
+import json
+import os
+
+# ログの設定
+logging.basicConfig(level=logging.INFO)
 
 # 東北大学が開発した日本語BERTモデルとトークナイザーのロード
 tokenizer = AutoTokenizer.from_pretrained('cl-tohoku/bert-base-japanese-whole-word-masking')
@@ -36,32 +43,104 @@ def compute_similarity(vector1, vector2):
 
     return similarity
 
+def save_dict_to_file(dictionary, file_path):
+    # Tensorをリストに変換
+    converted_dict = {key: value.cpu().numpy().tolist() for key, value in dictionary.items()}
+    
+    with open(file_path, 'w') as f:
+        json.dump(converted_dict, f)
+    
+def load_dict_from_file(file_path):
+    with open(file_path, 'r') as f:
+        dictionary = json.load(f)
+        # リストをTensorに変換
+        converted_dict = {key: torch.tensor(value) for key, value in dictionary.items()}
+        
+    return converted_dict
+
+def get_data(dbname):
+    command = 'SELECT * from theses'
+    db = Db(dbname)
+    data = db.db_output(command)
+    return data
+
 def main():
+    logging.info("During data acquisition and preprocessing...")
     # データの準備
     new_title = "量子コンピューティングにおける新たなアルゴリズム設計法とその応用"
-    new_content = """
+    new_content = \
+    """
     この論文では、量子コンピューティングにおける新しいアルゴリズム設計法を提案します。具体的には、従来の量子アルゴリズムが解決できる問題領域を広げるため、エンタングルメントとスーパーポジションを更に効果的に活用する新手法を開発しました。
 
-この新手法は、特に量子暗号、量子機械学習、量子最適化などの応用分野において優れた結果を示します。さらに、このアルゴリズム設計法を用いた新たなハイブリッド量子古典アルゴリズムを提案し、その計算効率とスケーラビリティについて評価します。
+    この新手法は、特に量子暗号、量子機械学習、量子最適化などの応用分野において優れた結果を示します。さらに、このアルゴリズム設計法を用いた新たなハイブリッド量子古典アルゴリズムを提案し、その計算効率とスケーラビリティについて評価します。
 
-最後に、この新しいアルゴリズム設計法が、既存の量子コンピューティング技術の限界を超えて、より広範で複雑な問題に対応できる可能性を示します。本論文は、量子コンピューティングのアルゴリズム開発とその応用分野への深い理解を促進することを目指しています。
-"""
+    最後に、この新しいアルゴリズム設計法が、既存の量子コンピューティング技術の限界を超えて、より広範で複雑な問題に対応できる可能性を示します。本論文は、量子コンピューティングのアルゴリズム開発とその応用分野への深い理解を促進することを目指しています。
+    """
+    
     dbname = './gpt-suggest/db/tuboroxn.db'
     data = get_data(dbname)
     
-    only_titles = [d[-2] for d in data]
-    only_contents = [d[-1] for d in data]
+    titles = [d[-2] for d in data]
+    contents = [d[-1] for d in data]
     
     
+    # 計算
+    new_title_vector = sentence_to_vector(new_title)
+    new_content_vector = sentence_to_vector(new_content)
+
+    title_vectors_file_path = "./theme-decision-support/vectors/title_vectors.json"
+    content_vectors_file_path = "./theme-decision-support/vectors/content_vectors.json"
+    
+    # タイトルのベクトルを計算して辞書に格納
+    if os.path.exists(title_vectors_file_path):
+        title_vectors = load_dict_from_file(title_vectors_file_path)
+    else:
+        title_vectors = {}
+        logging.info("vectorizing all titles...")
+        for title in tqdm(titles):
+            title_vectors[title] = sentence_to_vector(title)
+        save_dict_to_file(title_vectors, title_vectors_file_path)
+        
+    # 内容のベクトルを計算して辞書に格納
+    if os.path.exists(content_vectors_file_path):
+        content_vectors = load_dict_from_file(content_vectors_file_path)
+    else:
+        content_vectors = {}
+        logging.info("vectorizing all contents...")
+        for content in tqdm(contents):
+            content_vectors[content] = sentence_to_vector(content)
+        save_dict_to_file(content_vectors, content_vectors_file_path)
+        
+    # 比較計算
+    title_similarities = {}
+    content_similarities = {}
+    
+    logging.info("Calculating similarity between titles...")
+    for key, value in tqdm(title_vectors.items()):
+        title_similarities[key] = compute_similarity(new_title_vector, value)
+        title_similarities = dict(sorted(title_similarities.items(), key=lambda item: item[1]))
+        
+    logging.info("Calculating similarity between contents...")
+    for key, value in tqdm(content_vectors.items()):
+        content_similarities[key] = compute_similarity(new_content_vector, value)
+        content_similarities = dict(sorted(content_similarities.items(), key=lambda item: item[1]))
+        
+    # 結果を表示
+    print(f"title:{new_title}")
+    # 上位5つのkeyとvalueを表示
+    for i, (key, value) in enumerate(title_similarities.items()):
+        if i >= 5:
+            break
+        print(f"  Rank {i+1}: {key} - {value}")
     
 
-    
-
+    print(f"content:{new_content}")
+    # 上位5つのkeyとvalueを表示
+    for i, (key, value) in enumerate(content_similarities.items()):
+        if i >= 5:
+            break
+        print(f"  Rank {i+1}: {key} - {value}")
+        
+        
 if __name__ == "__main__":
-    # 2つの文章を定義
-    sentence1 = "私は犬が好きです。"
-    sentence2 = "それは機械学習を用いたメガネの学習です。"
-
-    # 類似度を計算
-    similarity = compute_similarity(sentence1, sentence2)
-    print(f"Similarity: {similarity}")
+    main()
