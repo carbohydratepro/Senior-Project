ac11e255bfdffdc206471cc3154fae871824c670
carbohydratepro <carbohydratepro@gmail.com>
2023-08-19 19:12:40 +0900

    メールを送信するプログラム

---
+++ collect_thesis/mail.py
@@ -0,0 +1,22 @@
+import smtplib
+from email.mime.text import MIMEText
+
+def send_email(subject, body):
+    from_email = "mcarbon660@gmail.com"
+    to_email = "masashigo0218@gmail.com"
+    
+    with open("./collect_thesis/count.txt", "r") as file:
+        password = file.read()  # 生成したApp Passwordを使用
+
+    msg = MIMEText(body)
+    msg["Subject"] = subject
+    msg["From"] = from_email
+    msg["To"] = to_email
+
+    server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+    server.login(from_email, password)
+    server.sendmail(from_email, to_email, msg.as_string())
+    server.quit()
+
+# 使用例
+send_email("Test Subject", "This is a test email.")
