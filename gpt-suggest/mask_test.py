b917d963819045927d762c90ebaf8569ae386342
carbohydratepro <carbohydratepro@gmail.com>
2023-09-25 00:06:25 +0900

    update

--- gpt-suggest/mask_test.py
+++ gpt-suggest/mask_test.py
@@ -5,8 +5,32 @@ from torch.optim import AdamW
 from janome.tokenizer import Tokenizer
 
 main_text = """
-最近世界中には、いろいろな犯罪事件がますます増えている。現在注目されている事件では家の侵入事件、盗難事件、殺害事件、火事、駐車場にある車のいたずらなどがある。
+国内における聴覚・言語障碍者は約34万人いるとされている．手話以外の会話
+方法として一般に筆談が用いられることがあるが、道具を必要とする点やリアルタ
+イム性に欠ける点を改善することは難しい．さらに遠隔でカメラ越しでの会話が普
+及し，他言語からの自動翻訳が求められている．その一方で手話の自動翻訳は普及
+が進まず，一般的に利用されているとはいえない．
+
+本研究では被識別者（手話話者）を正面から写すことのできる，1台のカメラに
+よる動画の手話識別を目的としてシステムの検討を行う．提案システムの特徴とし
+て関節軌跡情報の透過率を変化させることで手話の一連動作を識別可能としている．
+また，関節情報のみを用いることで，話者の服装や背景などの環境に左右されない
+システムとなっている．
+
+提案システムの流れとして，一連の手話動作画像からOpenPoseを用いて関節
+座標を取得する．その座標から軌跡として画像に出力したものを学習データとする．
+生成した画像の分類はCNNを利用したモデルで行う.本提案では一連の動作の中
+で動きのあるものを特徴として抽出できる点，色情報として時間方向の情報を保持
+できる点が挙げられる．さらに画像識別において不要な情報を排除することができ，
+識別率の向上が見込める．本研究ではその画像を左右の手と全身の関節画像に分け
+てCNNモデルに入力することで，11種類のクラス分類について学習データと同じ
+環境で95%，一連の手話動画による検証では87.8%の高い精度を得ることができた．
+また，学習に用いなかった他被験者3人での手話識別についても80.1%の識別率が
+得られた．このことから一人の手話を学習させたモデルにおいて他者での識別も可
+能であると示すことができた．今後は学習データを拡充させ，さらに多くのクラス
+でも手話識別が可能であるか検証を行う必要がある．
 """
+
 
 def analyze_text(text):
     t = Tokenizer()
@@ -15,9 +39,23 @@ def analyze_text(text):
     tokens_list = []
     tokens_with_pos_dict = {}
     
-    for token in t.tokenize(text):
-        tokens_list.append(token.surface)
-        tokens_with_pos_dict[token.surface] = token.part_of_speech.split(',')[0]  # 品詞の大分類のみを取得
+    tokens = list(t.tokenize(text))
+    
+    # 名詞の結合処理
+    i = 0
+    while i < len(tokens):
+        token = tokens[i]
+        if token.part_of_speech.split(',')[0] == '名詞' and i+1 < len(tokens) and tokens[i+1].part_of_speech.split(',')[0] == '名詞':
+            compound_noun = token.surface
+            while i+1 < len(tokens) and tokens[i+1].part_of_speech.split(',')[0] == '名詞':
+                i += 1
+                compound_noun += tokens[i].surface
+            tokens_list.append(compound_noun)
+            tokens_with_pos_dict[compound_noun] = '名詞'
+        else:
+            tokens_list.append(token.surface)
+            tokens_with_pos_dict[token.surface] = token.part_of_speech.split(',')[0]
+        i += 1
         
     return tokens_list, tokens_with_pos_dict
 
---
+++ theme-decision-support/dependency_analysis.py
@@ -0,0 +1,40 @@
+import spacy
+from spacy import displacy
+
+# GiNZAをロード
+nlp = spacy.load("ja_ginza")
+
+# 解析するテキスト (例: 論文のタイトルや抄録)
+text = "関節情報の取得にOpenPoseを用いて、"
+
+# テキストを処理
+doc = nlp(text)
+
+# 係り受け関係を表示
+for token in doc:
+    print(f"{token.text} <--{token.dep_}-- {token.head.text}")
+
+# 係り受けツリーを視覚化（オプション）
+displacy.serve(doc, style="dep")
+
+
+# 係り受け関係 (Dependency Relations):
+
+# nsubj: 名詞主語。動詞の主語となる名詞です。
+# obj: 目的語。動詞の目的語となる名詞や代名詞です。
+# amod: 形容詞修飾語。名詞を修飾する形容詞です。
+# advmod: 副詞修飾語。動詞や形容詞、他の副詞を修飾する副詞です。
+# root: 係り受け木の根。通常、文の主要な動詞です。
+# 他にも多くの係り受け関係タグがあり、それらの完全なリストと説明はUniversal Dependencies documentationで見ることができます。
+
+# 品詞タグ (Part-of-Speech Tags):
+
+# NOUN: 名詞
+# VERB: 動詞
+# ADJ: 形容詞
+# ADV: 副詞
+# ADP: 接置詞や助詞（日本語では助詞に相当）
+# PRON: 代名詞
+# DET: 限定詞
+# PUNCT: 句読点
+# これらのタグは、単語の文法的なカテゴリを示します。品詞タグの完全なリストと説明はUniversal POS tags documentationで見ることができます。
